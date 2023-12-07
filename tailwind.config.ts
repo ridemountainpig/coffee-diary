bb623f76d5db60d48bdce3c0f7b753ca4fd97253
ridemountainpig <lin0905580802@gmail.com>
2023-12-07 16:13:35 +0800

    feat: add coffee-diary-svg.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,28 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            colors: {
+                "coffee-white": "#fefdf5",
+                "coffee-black": "#021928",
+            },
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            fontFamily: {
+                nunito: ["Nunito", "sans-serif"],
+                kodchasan: ["Kodchasan", "sans-serif"],
+                koho: ["KoHo", "sans-serif"],
+                gluten: ["Gluten", "sans-serif"],
+                "dancing-script": ["Dancing Script", "cursive"],
+            },
+            fontSize: {
+                "2xs": ".65rem",
+                "3xs": ".5rem",
+            },
+            borderWidth: {
+                "3": "3px",
             },
         },
     },
