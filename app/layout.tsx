6a62635f064f92b897ea2339daa53d5c16e153e5
ridemountainpig <lin0905580802@gmail.com>
2024-01-24 09:53:57 +0800

    feat: add meta data config

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,37 @@ export const metadata: Metadata = {
     title: "COFFEE DIARY",
     description:
         "Begin crafting your coffee diary, chronicling the unique path you traverse with each cup.",
+    keywords:
+        "coffee, diary, coffee diary, coffee journal, coffee tasting, coffee experience",
+    authors: [
+        {
+            name: "ridemountainpig",
+            url: "https://www.github.com/ridemountainpig",
+        },
+    ],
+    openGraph: {
+        type: "website",
+        url: "https://coffee-diary.com/",
+        title: "COFFEE DIARY",
+        description:
+            "Begin crafting your coffee diary, chronicling the unique path you traverse with each cup.",
+        images: [
+            {
+                url: "https://coffee-diary.com/coffee-diary.png",
+                width: 1200,
+                height: 630,
+                alt: "COFFEE DIARY",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "COFFEE DIARY",
+        description:
+            "Begin crafting your coffee diary, chronicling the unique path you traverse with each cup.",
+        creator: "@ridemountainpig",
+        images: ["https://coffee-diary.com/coffee-diary.png"],
+    },
 };
 
 export default function RootLayout({
