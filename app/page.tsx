62ba56085bae31ab2b5322ec9651ec5f78300d3e
ridemountainpig <lin0905580802@gmail.com>
2024-02-27 20:31:51 +0800

    feat: styling up coffee navbar with dynamic dock

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Diary } from "@/components/homepage/diary";
 import { Footer } from "@/components/footer";
 import { WhatIsCoffeeDiary } from "@/components/homepage/what-is-coffee-diary";
 import { HowToUse } from "@/components/homepage/how-to-use";
+import DynamicDock from "dynamic-dock";
 
 export default function Home() {
     return (
@@ -12,9 +13,15 @@ export default function Home() {
             <div className="bg-coffee-white h-screen">
                 <Navbar></Navbar>
                 <div className="flex justify-center h-[65%]">
-                    <div>
+                    <div className="h-fit block lg:hidden">
                         <Banner></Banner>
                         <CoffeeNavbar></CoffeeNavbar>
+                    </div>
+                    <div className="h-fit hidden lg:block">
+                        <Banner></Banner>
+                        <DynamicDock gapX={0} imageWidth={64}>
+                            <CoffeeNavbar></CoffeeNavbar>
+                        </DynamicDock>
                     </div>
                 </div>
             </div>
--- components/homepage/coffee-navbar.tsx
+++ components/homepage/coffee-navbar.tsx
@@ -13,18 +13,10 @@ export function CoffeeNavbar() {
                     transition={{ duration: 1, delay: 0.5 }}
                 >
                     <Image
-                        className="hidden md:block"
                         src="/coffee-bag.svg"
                         alt="coffee bag"
                         width={64}
                         height={64}
-                    />
-                    <Image
-                        className="block md:hidden"
-                        src="/coffee-bag.svg"
-                        alt="coffee bag"
-                        width={48}
-                        height={48}
                     />
                 </motion.div>
                 <motion.div
@@ -33,18 +25,10 @@ export function CoffeeNavbar() {
                     transition={{ duration: 1, delay: 0.6 }}
                 >
                     <Image
-                        className="hidden md:block"
                         src="/coffee-abrader.svg"
                         alt="coffee abrader"
                         width={64}
                         height={64}
-                    />
-                    <Image
-                        className="block md:hidden"
-                        src="/coffee-abrader.svg"
-                        alt="coffee abrader"
-                        width={48}
-                        height={48}
                     />
                 </motion.div>
                 <motion.div
@@ -53,18 +37,10 @@ export function CoffeeNavbar() {
                     transition={{ duration: 1, delay: 0.7 }}
                 >
                     <Image
-                        className="hidden md:block"
                         src="/coffee-cup.svg"
                         alt="coffee cup"
                         width={64}
                         height={64}
-                    />
-                    <Image
-                        className="block md:hidden"
-                        src="/coffee-cup.svg"
-                        alt="coffee cup"
-                        width={48}
-                        height={48}
                     />
                 </motion.div>
                 <motion.div
@@ -73,18 +49,10 @@ export function CoffeeNavbar() {
                     transition={{ duration: 1, delay: 0.8 }}
                 >
                     <Image
-                        className="hidden md:block"
                         src="/coffee-pot.svg"
                         alt="coffee pot"
                         width={64}
                         height={64}
-                    />
-                    <Image
-                        className="block md:hidden"
-                        src="/coffee-pot.svg"
-                        alt="coffee pot"
-                        width={48}
-                        height={48}
                     />
                 </motion.div>
             </div>
