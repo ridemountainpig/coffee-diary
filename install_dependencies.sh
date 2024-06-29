0dbe585e440a688c4cadb820c9870ba176466166
ridemountainpig <lin0905580802@gmail.com>
2024-06-30 01:15:04 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-apt-get update && \
-apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libgbm1 libasound2 libpangocairo-1.0-0 libxss1 libgtk-3-0 libxshmfence1 libglu1 && \
+# apt-get update && \
+# apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libgbm1 libasound2 libpangocairo-1.0-0 libxss1 libgtk-3-0 libxshmfence1 libglu1 && \
 ENV PUPPETEER_CACHE_DIR=/src/.cache/puppeteer && \
 COPY package.json* tsconfig.json* .npmrc* . && \
 COPY pnpm-lock.yaml* . && \
